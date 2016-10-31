679f04170fbdda8f1b09505d50e5ef488132503d
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-10-31 15:09:31 +0900

    Add webpage

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,43 @@
 'use strict'
-import gulp       from 'gulp'
-import sourcemaps from 'gulp-sourcemaps'
-import sass       from 'gulp-sass'
-import concat     from 'gulp-concat'
-import minify     from 'gulp-clean-css'
-import rename     from 'gulp-rename'
+import gulp        from 'gulp'
+import plumber     from 'gulp-plumber'
+import sourcemaps  from 'gulp-sourcemaps'
+import sass        from 'gulp-sass'
+import concat      from 'gulp-concat'
+import minify      from 'gulp-clean-css'
+import rename      from 'gulp-rename'
+import browserSync from 'browser-sync'
 
 const styles = [
-  './node_modules/normalize.css/normalize.css',
+  './node_modules/bootstrap/dist/css/bootstrap.css',
+  './node_modules/font-awesome/css/font-awesome.css',
   './src/*.scss'
 ]
 
 gulp.task('css', () => {
   gulp.src(styles)
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(concat('style.css'))
-    .pipe(minify())
+    // .pipe(minify())
     .pipe(rename((file) => { file.extname = '.min.css' }))
-    .pipe(sourcemaps.write('./map/'))
+    .pipe(sourcemaps.write('./maps/'))
     .pipe(gulp.dest('./'))
 })
 
-gulp.task('build', ['css'])
+gulp.task('font', () => {
+  gulp.src('./node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('./fonts/'))
+})
+
+gulp.task('serve', ['build'], () => {
+  browserSync.init({
+    server: { baseDir: './' }
+  })
+  gulp.watch(styles, ['css'])
+  gulp.watch(['./index.html', './style.min.css'])
+    .on('change', browserSync.reload)
+})
+
+gulp.task('build', ['css', 'font'])
