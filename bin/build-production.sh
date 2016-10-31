b12d5c4ecc2a74fa2d53b2df8fa62b281dfd1017
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-10-31 16:11:10 +0900

    Add production build script

---
+++ bin/build-production.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DESTINE="./dest/"
+
+npm install
+npm run build
+
+if [[ ! -d $DESTINE ]]; then
+    mkdir $DESTINE
+fi
+
+tar cvzf ./wakayamarb.org.tar.gz fonts/ images index.html style.min.css
