4936d710553955abbe3b81fc55b4a99a9f212fb0
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-11-05 06:00:07 +0900

    Fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -20,20 +20,11 @@ echo 'Deploying to GitHub page brnch..'
 GITHUB_BRANCH_TO_DEPLOY=$1
 
 rm -rf .git/
-echo 1
 git init
-echo 2
 git config --global user.name "wakayamarb"
 git config --global user.email "wakayamarb@travis-ci.org"
-echo 3
-git remote add origin "git@github.com:wakayamarb.wakayamarb.org.git"
-git remote -v
-echo 4
+git remote add origin "git@github.com:wakayamarb/wakayamarb.org.git"
 git checkout -b $GITHUB_BRANCH_TO_DEPLOY
-git branch
-echo 5
 git add .
 git commit -m "Deploy from travis"
-echo 6
 git push --force origin $GITHUB_BRANCH_TO_DEPLOY
-echo 7
