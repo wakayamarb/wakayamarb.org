202c2e8b4a6ae3f3d1b276fce2d8d95f6d1b3bc1
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-11-05 05:41:50 +0900

    Fix deoloy script

--- bin/keygen.sh
+++ bin/keygen.sh
@@ -18,13 +18,8 @@ if [[ -f ./id_ecdsa.pub ]]; then
   rm ./id_ecdsa.pub
 fi
 
-# ssh-keygen -t ecdsa -f ./id_ecdsa -q -N ""
-# travis encrypt-file ./id_ecdsa >/dev/null 2>&1
+ssh-keygen -t ecdsa -f ./id_ecdsa -q -N ""
+travis encrypt-file ./id_ecdsa
 
-ssh-keygen -t rsa -f ./id_rsa -q -N ""
-travis encrypt-file ./id_rsa >/dev/null 2>&1
-
-echo "The keys has been reset. Add the public key shown below at 'https://github.com/wakayamarb/wakayamarb.org/settings/keys' if you have grant to access the repository."
-echo "If you regenerate this public key, it is recommendable to remove the old key."
-echo ""
+echo "Public key:"
 cat ./id_ecdsa.pub
