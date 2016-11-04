695bbd2bf594be1f08bd08f0588b87bd40aeaaa3
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-11-04 13:54:59 +0900

    Fix build script

--- bin/production.sh
+++ bin/production.sh
@@ -9,4 +9,4 @@ if [[ ! -d $DESTINE ]]; then
     mkdir $DESTINE
 fi
 
-tar cvzf ./wakayamarb.org.tar.gz fonts/ images index.html style.min.css bundle.js
+tar cvzf ./wakayamarb.org.tar.gz fonts/ images index.html style.css bundle.js
