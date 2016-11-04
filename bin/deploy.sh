9357a7e9537edb5e939302d80e276dbfa118f93a
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-11-05 04:39:03 +0900

    Fix dev svripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,10 @@ if [[ $CI != "true" && $TRAVIS != "true" ]]; then
   echo 'Not callable witout travis CI.'
 fi
 
+if [[ $2 != "$3" ]]; then
+  echo "Not suitable to deploy. $2 expected to be $3."
+fi
+
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
   echo 'Not deploying from Pull Request.'
   exit 0
@@ -14,11 +18,13 @@ if [[ -d './git/' ]]; then
   rm -rf .git/
 fi
 
+GITHUB_BRANCH_TO_DEPLOY=$1
+
 git init
-git config user.name "wakayamarb"
-git config user.email "wakayamarb@travis-ci.org"
+git config user.name "$USER"
+git config user.email "$USER@travis-ci.org"
 git remote add origin "git@github.com:$TRAVIS_REPO_SLUG.org.git"
-git checkout -b gh-pages
+git checkout -b $GITHUB_BRANCH_TO_DEPLOY
 git add .
 git commit -m"Deploy from travis"
-git push --force origin gh-pages
+git push --force origin $GITHUB_BRANCH_TO_DEPLOY
--- bin/keygen.sh
+++ bin/keygen.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo 'Are you sure that this command rests deploy key? [y/N]'
+read RESET
+if [[ $RESET != 'y' ]]; then
+  exit 0
+fi
+
 if [[ -f ./id_ecdsa ]]; then
   rm ./id_ecdsa
 fi
@@ -15,7 +21,8 @@ fi
 ssh-keygen -t ecdsa -f ./id_ecdsa -q -N ""
 travis encrypt-file ./id_ecdsa >/dev/null 2>&1
 
-echo "Add the public key below at 'https://github.com/wakayamarb/wakayamarb.org/settings/keys' if you have grant to access the repository."
+echo "The keys has been reset."
+echo "Add the public key shown below at 'https://github.com/wakayamarb/wakayamarb.org/settings/keys' if you have grant to access the repository."
 echo "If you regenerate this public key, it is recommendable to remove the old key."
 
 cat ./id_ecdsa.pub
